d2fb9589cdd90a8305e67ae4cbd0313ee4b1bcd9
Reza Arbabi <rarbabi@rarbabi-mn2.linkedin.biz>
2019-11-06 23:41:04 -0800

    testing modules

---
+++ module/car.js
@@ -0,0 +1,34 @@
+class Car {
+    constructor (name) {
+        this.name = name;
+        this.power = 0;
+    }
+
+    horsepower (power) {
+        this.power = power;
+        console.log(`${this.name} has ${this.power} horsepower!`);
+    }
+}
+
+class Jaguar extends Car {
+    constructor(name, color) {
+        super(name);
+        this._color = color;
+    }
+
+    get color() {
+        return this._color;
+    }
+
+    set color(newColor) {
+        this._color = newColor;
+    }
+
+    toString() {
+        return `${this.name}_${this.color}_${this.power}`;
+    }
+}
+
+let jag = new Jaguar("LX", "Green");
+jag.horsepower(360);
+console.log(jag.toString());
---
+++ module/main.mjs
@@ -0,0 +1,6 @@
+'use strict';
+import Car from './car.js';
+
+let jag = new Jaguar("LX", "Green");
+jag.horsepower(360);
+console.log(jag.toString());
